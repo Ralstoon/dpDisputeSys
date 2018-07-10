0ea769bfc0b25afcbccf473bcbf23e551a9d26d2
wangj <wangj1106@qq.com>
2018-07-10 14:50:04 +0800

    first commit init project

---
+++ src/main/java/com/seu/DpdisputesysApplication.java
@@ -0,0 +1,14 @@
+package com.seu;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.bingo.repository")
+public class DpdisputesysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpdisputesysApplication.class, args);
+	}
+}
