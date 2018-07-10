ee2b10f3ce1239320c61ad8b892f9d559d85db04
wangj <wangj1106@qq.com>
2018-07-10 19:10:56 +0800

    更新

--- src/main/java/com/seu/service/impl/NormalUserImpl.java
+++ src/main/java/com/seu/service/impl/NormalUserImpl.java
@@ -1,13 +1,13 @@
 package com.seu.service.impl;
 
-import com.seu.common.ServerResponse;
-import com.seu.domian.NormalUser;
-import com.seu.repository.NormalUserRepository;
-import com.seu.service.INormalUserService;
-import com.seu.util.MD5Util;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+        import com.seu.common.ServerResponse;
+        import com.seu.domian.NormalUser;
+        import com.seu.repository.NormalUserRepository;
+        import com.seu.service.INormalUserService;
+        import com.seu.util.MD5Util;
+        import org.apache.commons.lang3.StringUtils;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
 
 @Service
 public class NormalUserImpl implements INormalUserService {
