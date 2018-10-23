879aa8b273d193f5b96d7c87ed10e53c90bfb46f
Ralstoon <15895923029@163.com>
2018-10-23 21:58:27 +0800

    tem

--- src/main/java/com/seu/elasticsearch/ESConstants.java
+++ src/main/java/com/seu/elasticsearch/ESConstants.java
@@ -9,5 +9,5 @@ package com.seu.elasticsearch;
  **/
 public interface ESConstants {
     String clusterName="disputebigdata";
-    String IP="223.3.72.234";  // 目前是这个
+    String IP="139.199.166.125";  // 目前是这个
 }
