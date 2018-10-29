28e5a10dad5e49dfeaa71c640c3970d0f6e2a912
Wang Jie <wangj1106@qq.com>
2018-10-29 13:45:18 +0800

    文件存储

--- src/main/java/com/seu/utils/GetWorkingTimeUtil.java
+++ src/main/java/com/seu/utils/GetWorkingTimeUtil.java
@@ -82,7 +82,7 @@ public class GetWorkingTimeUtil {
         if (EndDate.isWeekday==null){
 
             FileInputStream freader;
-            freader = new FileInputStream("E:/1.txt");
+            freader = new FileInputStream("/home/ubuntu/isWeekday.txt");
             ObjectInputStream objectInputStream = new ObjectInputStream(freader);
 
             EndDate.isWeekday = (Map<Date, Integer>) objectInputStream.readObject();
